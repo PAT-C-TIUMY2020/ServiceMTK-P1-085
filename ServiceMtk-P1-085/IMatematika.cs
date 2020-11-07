8ce274077ad2613a573f729063d5893ceff5d7fb
Mochammad Ifan Rizky Kurniawan <62989237+IfanRizky@users.noreply.github.com>
2020-11-07 14:01:19 +0700

    add FaultContract

--- ServiceMtk-P1-085/IMatematika.cs
+++ ServiceMtk-P1-085/IMatematika.cs
@@ -12,15 +12,24 @@ namespace ServiceMtk_P1_085
     public interface IMatematika
     {
         [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exception
         /* output->*/
         int Tambah(int a, int b); //method
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exception
         int Kurang(int a, int b); //input
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exception
         int Kali(int a, int b);
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exception
         int Bagi(int a, int b);
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exception
         Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class
     }
 
