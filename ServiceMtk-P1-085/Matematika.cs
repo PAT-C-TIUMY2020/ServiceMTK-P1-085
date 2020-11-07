970386bc44bf6855e270ac3055a3906386accbdc
Mochammad Ifan Rizky Kurniawan <62989237+IfanRizky@users.noreply.github.com>
2020-11-07 14:23:31 +0700

    add try catch in Koordinat

--- ServiceMtk-P1-085/Matematika.cs
+++ ServiceMtk-P1-085/Matematika.cs
@@ -100,11 +100,24 @@ namespace ServiceMtk_P1_085
 
         public Koordinat TKoordinat(Koordinat a, Koordinat b)
         {
+            try
+            { 
             Koordinat hasil = new Koordinat();
             hasil.X = a.X + b.X;
             hasil.Y = a.Y + b.Y;
             return hasil;
-        }
+        } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //secara default
+                //throw new Exception(ex.Message) //pesan error secara default
+                //throw new FaultException("Pesan Salah"); //untuk meloloskan pesan error yang dikirim ke client
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan Yang Anda Masukan Salah";
+                mf.Pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
 
     }
 }
